4f48b295e60c6ed229b68712a549ca7919c53290
QamarJaradat <moon19.qs@gmail.com>
2020-12-28 15:55:55 +0200

    try to deploy

--- frontend/src/comp/main.jsx
+++ frontend/src/comp/main.jsx
@@ -5,12 +5,17 @@ export default class main extends Component {
     render() {
         return (
             <div>
-                <ul style={{ position: 'fixed', width: '100%' }}>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#news">News</a></li>
+                <ul className='sticky'>
+                    <li><a href="#main">Home</a></li>
+                    <li><a href="#home">About Me</a></li>
+                    <li><a href="#news">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
-                    <li style={{ float: 'right' }}><a className="active" href="#about">About</a></li>
+                    <li style={{ float: 'right' }}><a className="active" href="#about">Contact</a></li>
                 </ul>
+                <div id='main' className='maindiv' style={{ paddingTop: '150px' }}>
+                    <div className='inmaindiv' ></div>
+                    <div id='triangle-bottomleft'></div>
+                </div>
                 <div id='home'>home</div>
                 <div id='news'>neeeeee</div>
                 <div id='contact'>hhhh</div>
